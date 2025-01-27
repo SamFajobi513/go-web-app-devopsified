e7816234c655575b3565b37a2dcaab607aa75eb3
SamFajobi <olusegunfajobi12@gmail.com>
2025-01-28 00:25:12 +0100

    gomain file update

--- main.go
+++ main.go
@@ -9,3 +9,13 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	// Render the home html page from static folder
 	http.ServeFile(w, r, "static/home.html")
 }
+
+func coursePage(w http.ResponseWriter, r *http.Request) {
+	// Render the course html page
+	http.ServeFile(w, r, "static/courses.html")
+}
+
+func aboutPage(w http.ResponseWriter, r *http.Request) {
+	// Render the about html page
+	http.ServeFile(w, r, "static/about.html")
+}
