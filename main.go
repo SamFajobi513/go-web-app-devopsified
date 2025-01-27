c2f8ecef3d8197c9db3f94dae919000cd78b34d1
SamFajobi <olusegunfajobi12@gmail.com>
2025-01-28 00:26:24 +0100

    go main file update

--- main.go
+++ main.go
@@ -19,3 +19,21 @@ func aboutPage(w http.ResponseWriter, r *http.Request) {
 	// Render the about html page
 	http.ServeFile(w, r, "static/about.html")
 }
+
+func contactPage(w http.ResponseWriter, r *http.Request) {
+	// Render the contact html page
+	http.ServeFile(w, r, "static/contact.html")
+}
+
+func main() {
+
+	http.HandleFunc("/home", homePage)
+	http.HandleFunc("/courses", coursePage)
+	http.HandleFunc("/about", aboutPage)
+	http.HandleFunc("/contact", contactPage)
+
+	err := http.ListenAndServe("0.0.0.0:8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
