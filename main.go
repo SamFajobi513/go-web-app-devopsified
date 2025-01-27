fd80a8026cf19926d7097d5309ba00a65b5cd0c9
SamFajobi <olusegunfajobi12@gmail.com>
2025-01-28 00:24:14 +0100

    main go file update

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	// Render the home html page from static folder
+	http.ServeFile(w, r, "static/home.html")
+}
